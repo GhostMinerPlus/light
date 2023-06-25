9af0c35c1723481b06ef7e3ba1c4beb6b32c259d
ghostminer <1696415745@qq.com>
2023-06-26 00:24:19 +0800

    replace setting proxies with setting hosts in config

--- commit.md
+++ commit.md
@@ -1,2 +1,5 @@
+# replace setting proxies with setting hosts in config
+it will be more convenient for a new server
+
 # rename Proxy.name to Proxy.path
 it shouldn't be named that
--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,10 @@ static mut APP: Option<App> = None;
 
 fn main() {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-
     let mut config = Config::default();
     earth::Config::merge_by_file(&mut config, "earth.toml");
     earth::Config::merge_by_args(&mut config, &std::env::args().collect());
-
-    App::create_app(config).run();
+    App::start_app(config);
 }
 
 #[derive(serde::Deserialize, serde::Serialize)]
@@ -24,18 +22,17 @@ pub(crate) struct Config {
     domain: String,
     path: String,
     src: String,
-
-    proxies: BTreeMap<String, String>,
+    hosts: Vec<String>,
 }
 
 impl Default for Config {
     fn default() -> Self {
         Self {
             name: "Light".to_string(),
-            domain: "[::]:8080".to_string(),
+            domain: "[::1]:8080".to_string(),
             path: "/light".to_string(),
             src: ".".to_string(),
-            proxies: BTreeMap::new(),
+            hosts: Vec::new(),
         }
     }
 }
@@ -56,46 +53,56 @@ impl App {
         unsafe { APP.as_ref().unwrap() }
     }
 
-    fn create_app(config: Config) -> &'static mut App {
-        unsafe {
-            APP = Some(App {
-                name: config.name,
-                domain: config.domain,
-                path: config.path,
-                src: config.src,
-                proxies: Mutex::new(config.proxies),
+    fn start_app(config: Config) {
+        tokio::runtime::Builder::new_multi_thread()
+            .enable_all()
+            .build()
+            .unwrap()
+            .block_on(async {
+                let this = unsafe {
+                    APP = Some(App {
+                        name: config.name,
+                        domain: config.domain,
+                        path: config.path,
+                        src: config.src,
+                        proxies: Mutex::new(BTreeMap::new()),
+                    });
+                    APP.as_mut().unwrap()
+                };
+                this.register_hosts(&config.hosts).await;
+                this.start_http_service().await;
             });
-            APP.as_mut().unwrap()
+    }
+
+    async fn register_hosts(&self, hosts: &Vec<String>) {
+        let client = reqwest::Client::new();
+        let proxy = serde_json::to_string(&application::dto::Proxy {
+            path: self.path.clone(),
+            url: format!("http://{}{}", self.domain, self.path),
+        })
+        .unwrap();
+        for host in hosts {
+            client
+                .post(format!("{host}/system/add_proxy"))
+                .header(reqwest::header::CONTENT_TYPE, "application/json")
+                .body(proxy.clone())
+                .send()
+                .await
+                .unwrap();
         }
     }
 
-    fn run(&mut self) {
-        self.start_http_service();
-    }
-
-    fn start_http_service(&self) {
+    async fn start_http_service(&self) {
         log::info!("start http service");
-        let domain = Self::get_app().domain.clone();
-        let path = Self::get_app().path.clone();
+        let domain = self.domain.clone();
+        let path = self.path.clone();
         log::info!("http service serves at: http://{domain}{path}");
         let server = HttpServer::new(move || {
             actix_web::App::new()
                 .wrap(interfaces::http::Proxy {})
                 .service(actix_web::web::scope(&path).configure(interfaces::http::config))
         });
-        let _ = tokio::runtime::Builder::new_multi_thread()
-            .enable_all()
-            .build()
-            .unwrap()
-            .block_on(
-                match server.bind(&domain) {
-                    Ok(server) => server,
-                    Err(e) => {
-                        log::error!("{:?}", e);
-                        return;
-                    }
-                }
-                .run(),
-            );
+
+        server.bind(&domain).unwrap().run().await.unwrap();
     }
 }
