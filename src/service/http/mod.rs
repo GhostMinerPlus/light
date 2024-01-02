fb7a2bddb992254d213afc2a7ed53605424b4379
ghostminer <1696415745@qq.com>
2024-01-03 01:28:00 +0800

    # default service

--- src/service/http/mod.rs
+++ src/service/http/mod.rs
@@ -1,6 +1,6 @@
 pub mod service;
 
-use actix_files::Files;
+use actix_files::{Files, NamedFile};
 use actix_http::{body::BoxBody, Payload};
 use actix_web::{
     dev::{forward_ready, Service, Transform},
@@ -159,11 +159,24 @@ where
 }
 
 fn config(path: &str, src: &str) -> impl HttpServiceFactory {
+    let src = src.to_string();
     actix_web::web::scope(&path)
         .service(service::system::add_proxy)
         .service(service::system::remove_proxy)
         .service(service::system::list_proxies)
-        .service(Files::new("", src).index_file("index.html"))
+        .service(
+            Files::new("", &src)
+                .index_file("index.html")
+                .default_handler(actix_web::dev::fn_service(move |req: ServiceRequest| {
+                    let index_html = format!("{}/index.html", src);
+                    let (req, _) = req.into_parts();
+                    async {
+                        let file = NamedFile::open_async(index_html).await?;
+                        let res = file.into_response(&req);
+                        Ok(ServiceResponse::new(req, res))
+                    }
+                })),
+        )
 }
 
 // public
