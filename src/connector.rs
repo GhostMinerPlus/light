ff289ece91a1a9c09e252992c6fb7a52409c585a
ghostminer <1696415745@qq.com>
2024-07-06 10:03:19 +0800

    update edge_lib

--- src/connector.rs
+++ src/connector.rs
@@ -1,6 +1,6 @@
 use std::{io, sync::Arc, time::Duration};
 
-use edge_lib::{data::AsDataManager, util::Path, EdgeEngine, ScriptTree};
+use edge_lib::{data::AsDataManager, EdgeEngine, ScriptTree};
 use tokio::time;
 
 use crate::util;
@@ -17,7 +17,7 @@ impl HttpConnector {
     pub async fn run(self) -> io::Result<()> {
         loop {
             if let Err(e) = self.execute().await {
-                log::warn!("when run:\n{e}");
+                log::warn!("{e}\nwhen run");
             }
 
             time::sleep(Duration::from_secs(10)).await;
@@ -25,8 +25,10 @@ impl HttpConnector {
     }
 
     async fn execute(&self) -> io::Result<()> {
-        let mut edge_engine = EdgeEngine::new(self.dm.clone());
+        let ip = util::native::get_global_ipv6()
+            .map_err(|e| io::Error::other(format!("{e}\nwhen execute")))?;
 
+        let mut edge_engine = EdgeEngine::new(self.dm.clone());
         let rs = edge_engine
             .execute1(&ScriptTree {
                 script: [
@@ -42,14 +44,6 @@ impl HttpConnector {
             .map_err(|e| io::Error::other(format!("{e}\nwhen execute")))?;
         log::debug!("{rs}");
         let name = rs["info"][0].as_str().unwrap();
-        let ip = {
-            let domain_v = self.dm.get(&Path::from_str("root->domain")).await?;
-            if !domain_v.is_empty() && !domain_v[0].is_empty() {
-                domain_v[0].clone()
-            } else {
-                util::native::get_global_ipv6()?
-            }
-        };
         let port = rs["info"][1].as_str().unwrap();
         let path = rs["info"][2].as_str().unwrap();
 
@@ -65,42 +59,90 @@ impl HttpConnector {
         let moon_server_v = &rs["moon_server"];
 
         let script = [
-            &format!("$->$server_exists = inner root->web_server {name}<-name"),
-            "$->$web_server = if $->$server_exists ?",
-            &format!("$->$web_server->name = = {name} _"),
-            &format!("$->$web_server->ip = = {ip} _"),
-            &format!("$->$web_server->port = = {port} _"),
-            &format!("$->$web_server->path = = {path} _"),
-            "root->web_server += left $->$web_server $->$server_exists",
+            &format!("$->$server_exists inner root->web_server {name}<-name"),
+            "$->$web_server if $->$server_exists ?",
+            &format!("$->$web_server->name = {name} _"),
+            &format!("$->$web_server->ip = {ip} _"),
+            &format!("$->$web_server->port = {port} _"),
+            &format!("$->$web_server->path = {path} _"),
+            "$->$web_server left $->$web_server $->$server_exists",
+            "root->web_server append root->web_server $->$web_server",
         ]
         .join("\\n");
         for moon_server in moon_server_v.members() {
             let uri = match moon_server.as_str() {
                 Some(uri) => uri,
                 None => {
-                    log::error!("when execute:\nfailed to parse uri for moon_server");
+                    log::error!("failed to parse uri for moon_server\nwhen execute");
                     continue;
                 }
             };
             log::info!("reporting to {uri}");
-            if let Err(e) = util::http_execute(&uri, format!("{{\"{script}\": null}}")).await {
-                log::warn!("when execute:\nwhen http_execute:\n{e}");
-                if let Err(e) = util::http_execute1(
-                    &uri,
-                    &ScriptTree {
-                        script: script.replace("\\n", "\n"),
-                        name: format!("info"),
-                        next_v: vec![],
-                    },
-                )
-                .await
-                {
-                    log::warn!("when execute:\nwhen http_execute1:\n{e}");
-                }
+            if let Err(e) = util::http_execute1(
+                &uri,
+                &ScriptTree {
+                    script: script.clone(),
+                    name: format!("info"),
+                    next_v: vec![],
+                },
+            )
+            .await
+            {
+                log::warn!("{e}\nwhen execute");
             } else {
                 log::info!("reported to {uri}");
             }
         }
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+
+    use edge_lib::{
+        data::{AsDataManager, Auth, MemDataManager},
+        util::Path,
+        EdgeEngine, ScriptTree,
+    };
+
+    #[test]
+    fn test() {
+        tokio::runtime::Builder::new_multi_thread()
+            .worker_threads(4)
+            .enable_all()
+            .build()
+            .unwrap()
+            .block_on(async {
+                let dm = Arc::new(MemDataManager::new(Auth::printer("root")));
+                let mut edge_engine = EdgeEngine::new(dm.clone());
+                // config.ip, config.port, config.name
+                let name = "test";
+                let ip = "0.0.0.0";
+                let port = "8080";
+                let path = "/test";
+                let script = [
+                    &format!("$->$server_exists = inner root->web_server {name}<-name"),
+                    "$->$web_server = if $->$server_exists ?",
+                    &format!("$->$web_server->name = {name} _"),
+                    &format!("$->$web_server->ip = {ip} _"),
+                    &format!("$->$web_server->port = {port} _"),
+                    &format!("$->$web_server->path = {path} _"),
+                    "root->web_server += left $->$web_server $->$server_exists",
+                ]
+                .join("\n");
+                edge_engine
+                    .execute1(&ScriptTree {
+                        script,
+                        name: format!("info"),
+                        next_v: vec![],
+                    })
+                    .await
+                    .unwrap();
+                edge_engine.commit().await.unwrap();
+                let rs = dm.get(&Path::from_str("root->web_server")).await.unwrap();
+                assert!(!rs.is_empty());
+            })
     }
 }
